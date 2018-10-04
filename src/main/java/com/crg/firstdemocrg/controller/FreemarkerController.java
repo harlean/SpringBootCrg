afe61cd4b26e34fb83eab42b8d93c9fb0173b781
harlean <1065008044@qq.com>
2018-10-04 15:06:00 +0800

    service

--- src/main/java/com/crg/firstdemocrg/controller/FreemarkerController.java
+++ src/main/java/com/crg/firstdemocrg/controller/FreemarkerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("ftl")
+@RequestMapping("/ftl")
 public class FreemarkerController {
 
 	@Autowired
---
+++ src/main/java/com/crg/firstdemocrg/controller/RedisController.java
@@ -0,0 +1,75 @@
+package com.crg.firstdemocrg.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.crg.firstdemocrg.domin.IMoocJSONResult;
+import com.crg.firstdemocrg.domin.SysUser;
+import com.crg.firstdemocrg.domin.User;
+import com.crg.firstdemocrg.until.JsonUtils;
+import org.n3r.idworker.utils.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("redis")
+public class RedisController {
+	
+	@Autowired
+	private StringRedisTemplate strRedis;
+	
+	@Autowired
+	private RedisOperator redis;
+	
+	@RequestMapping("/test")
+	public IMoocJSONResult test() {
+		
+		strRedis.opsForValue().set("imooc-cache", "hello 慕课网~~~~~~");
+		
+		SysUser user = new SysUser();
+		user.setId("100111");
+		user.setNickname("imooc");
+		user.setPassword("abc123");
+		strRedis.opsForValue().set("json:user", JsonUtils.objectToJson(user));
+		SysUser jsonUser = JsonUtils.jsonToPojo(strRedis.opsForValue().get("json:user"), SysUser.class);
+		return IMoocJSONResult.ok(jsonUser);
+	}
+	
+	@RequestMapping("/getJsonList")
+	public IMoocJSONResult getJsonList() {
+
+		User user = new User();
+		user.setAge(18);
+		user.setName("慕课网");
+		user.setPassword("123456");
+		user.setBirthday(new Date());
+
+		User u1 = new User();
+		u1.setAge(19);
+		u1.setName("imooc");
+		u1.setPassword("123456");
+		u1.setBirthday(new Date());
+
+		User u2 = new User();
+		u2.setAge(17);
+		u2.setName("hello imooc");
+		u2.setPassword("123456");
+		u2.setBirthday(new Date());
+
+		List<User> userList = new ArrayList<>();
+		userList.add(user);
+		userList.add(u1);
+		userList.add(u2);
+
+		redis.set("json:info:userlist", JsonUtils.objectToJson(userList), 2000);
+		String userListJson = redis.get("json:info:userlist");
+		List<User> userListBorn = JsonUtils.jsonToList(userListJson, User.class);
+
+		return IMoocJSONResult.ok(userListBorn);
+	}
+}
